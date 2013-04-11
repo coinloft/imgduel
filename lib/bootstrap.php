8b7cdd3dbfeb80f5265330890e893576fba052af
Steve High <steve.high@gmail.com>
2013-04-10 20:52:20 -0400

    App skeleton and bootstrap set up

---
+++ lib/bootstrap.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ *  NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ *  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *  bootstrap.php
+ *  Build A Social App In PHP
+ *  SkillShare/Start It Up Delaware/The coIN Loft
+ *  Created:    2013-03-30
+ *  Modified:   0000-00-00
+ */
+
+//  FLAGS
+/*********************************************************************************/
+//  the bootstrap is loaded.
+define ('IMGDUEL_BOOSTRAP', true);
+
+//  PATHS
+/*********************************************************************************/
+//  Root path
+define ('IMGDUEL_ROOT_PATH', realpath(getcwd() . DIRECTORY_SEPARATOR . '..'));
+//  Lib path
+define ('IMGDUEL_LIB_PATH', IMGDUEL_ROOT_PATH . DIRECTORY_SEPARATOR . 'lib');
+//  Class path
+define ('IMGDUEL_CLASS_PATH', IMGDUEL_LIB_PATH . DIRECTORY_SEPARATOR . 'classes');
+//  Config Path
+define ('IMGDUEL_CONF_PATH', IMGDUEL_ROOT_PATH . DIRECTORY_SEPARATOR . 'conf');
+//  Site Root
+define ('IMGDUEL_WWW_PATH', IMGDUEL_ROOT_PATH . DIRECTORY_SEPARATOR . 'www');
+
+//  INCLUDES
+/*********************************************************************************/
+require IMGDUEL_LIB_PATH . DIRECTORY_SEPARATOR . 'functions.php';
---
+++ lib/functions.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ *  NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ *  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *  functions.php
+ *  Build A Social App In PHP
+ *  SkillShare/Start It Up Delaware/The coIN Loft
+ *  Created:    2013-03-30
+ *  Modified:   0000-00-00
+ */
+
+/**
+ * Loads a class from the classpath
+ * @param $classname
+ */
+function imgduel_load_class($classname)
+{
+    $path = sprintf('%s%s%s.php', IMGDUEL_CLASS_PATH, DIRECTORY_SEPARATOR, str_replace('_', DIRECTORY_SEPARATOR, $classname));
+    require_once $path;
+}
+
+/**
+ * Gets a path to a config file
+ * @param $filename The name of the config file
+ * @param string $suffix The suffix of the config file
+ * @return string The full Path to the config file
+ */
+function imgduel_config_file($filename, $suffix = 'ini')
+{
+    return sprintf('%s%s%s.%s', IMGDUEL_CONF_PATH, DIRECTORY_SEPARATOR, $filename, $suffix);
+}
+
+/**
+ * Gets the full path to the default config file based on hostname
+ * @return string
+ */
+function imgduel_default_config()
+{
+    return imgduel_config_file(php_uname('n'));
+}
